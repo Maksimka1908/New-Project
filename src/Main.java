11ec00c0b51e2d20992c89b61b1d242ed7501a28
Максим <gud.maks999@gmail.com>
2024-06-19 12:56:46 +0300

    Added a line in the code with the text "Внесение изменений"

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Первый commit");
         System.out.println("Создание ветки");
+        System.out.print("Внесение изменений!");
     }
 }
